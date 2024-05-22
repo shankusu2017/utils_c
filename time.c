3d7ee86c7e72bf5f8773205fd66650f8caa2215a
shankusu <shankusu2017@gmail.com>
2024-05-22 16:11:52 +0800

    feature: 毫秒、微妙级别的定时器

---
+++ time.c
@@ -0,0 +1,31 @@
+#include <sys/select.h>
+#include "time.h"
+
+/* 毫秒级别定时器 */
+void utils_msleep(unsigned long milli_second)
+{
+    struct timeval tv;
+
+    tv.tv_sec = milli_second / 1000;
+    tv.tv_usec = (milli_second % 1000) * 1000;
+	
+    int err = 0;
+    do{
+       err = select(0, NULL, NULL, NULL, &tv);
+    } while (err < 0 && errno == EINTR);
+}
+
+/* 微妙级别定时器 */
+void utils_usleep(long micro_second)
+{
+	struct timeval tv;
+
+    tv.tv_sec	=	micro_second / 1000000;
+    tv.tv_usec	=	micro_second % 1000000;
+    int err = 0;
+	
+    do {
+       	err = select(0, NULL, NULL, NULL, &tv);
+    } while (err < 0 && errno == EINTR);
+}
+
---
+++ time.h
@@ -0,0 +1,9 @@
+#ifndef TIME_H_0X34B32598
+#define TIME_H_0X34B32598
+
+/* 毫秒级别定时器 */
+void utils_msleep(unsigned long milli_second);
+/* 微妙级别定时器 */
+void utils_usleep(long micro_second);
+
+#endif /* TIME_H_0X34B32598 */
