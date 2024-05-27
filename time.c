c275c8b4bb1e59993df5a10da252ef08f0eeaeb7
shankusu <shankusu2017@gmail.com>
2024-05-27 14:42:58 +0800

    little

--- time.c
+++ time.c
@@ -8,7 +8,7 @@ void utils_msleep(unsigned long milli_second)
 
     tv.tv_sec = milli_second / 1000;
     tv.tv_usec = (milli_second % 1000) * 1000;
-	
+
     int err = 0;
     do{
        err = select(0, NULL, NULL, NULL, &tv);
@@ -23,7 +23,7 @@ void utils_usleep(long micro_second)
     tv.tv_sec	=	micro_second / 1000000;
     tv.tv_usec	=	micro_second % 1000000;
     int err = 0;
-	
+
     do {
        	err = select(0, NULL, NULL, NULL, &tv);
     } while (err < 0 && errno == EINTR);
