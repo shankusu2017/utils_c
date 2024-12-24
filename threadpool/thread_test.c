0b1c5a9722f4fac9ad902dfffa2d62f603f64376
shankusu <shankusu2017@gmail.com>
2024-12-24 18:29:34 +0800

    代码优化：适配最新的 uc_threadpool代码。

--- threadpool/thread_test.c
+++ threadpool/thread_test.c
@@ -15,16 +15,6 @@ static void *print_arg(void *arg)
     return NULL;
 }
 
-static void *print_arg_seq(void *arg)
-{
-    static int seq = 0;
-    int *pi = (int *)arg;
-    printf("arg: %d\n", *pi);
-    assert(seq++ == *pi);       /* 任务被按序执行 */
-    test_arr_seq[*pi] = *pi;    /* mark run data*/
-    return NULL;
-}
-
 static int test_thread_number(void)
 {
     uc_threadpool_t *th = uc_threadpool_create(1, 128, sizeof(int));
@@ -34,14 +24,13 @@ static int test_thread_number(void)
     }
 
     for (int i = 0; i < 512; ++i) {
-        uc_threadpool_task_t *task = uc_threadpool_malloc_fixed_task(th);
+        int *task = malloc(sizeof(*task));
         if (NULL == task) {
             printf("malloc task fail\n");
             exit(-1);
         }
-        task->fun = print_arg;
-        *((int *)task->arg) = i;
-        uc_threadpool_add_fixed_task(th, task);
+        *task = i;
+        uc_threadpool_add_void_task(th, print_arg, task);
     }
 
     uc_threadpool_wait_task_done(th);
@@ -59,46 +48,9 @@ static int test_thread_number(void)
     return 0;
 }
 
-static int test_thread_seq(void)
-{
-    uc_threadpool_t *th = uc_threadpool_create(1, 1, sizeof(int));
-    if (!th) {
-        printf("create threadpool fail\n");
-        exit(-1);
-    }
-
-    for (int i = 0; i < 1024; ++i) {
-        uc_threadpool_task_t *task = uc_threadpool_malloc_fixed_task(th);
-        if (NULL == task) {
-            printf("malloc task fail\n");
-            exit(-1);
-        }
-        task->fun = print_arg_seq;
-        *((int *)task->arg) = i;
-        uc_threadpool_add_fixed_task(th, task);
-    }
-
-    uc_threadpool_wait_task_done(th);
-
-    printf("thread pool close start\n");
-    uc_threadpool_close(th);
-    printf("thread pool close end\n");
-    sleep(1);
-
-    printf("thread pool free done\n");
-    printf("========> 0x512d8088 check thread task do start\n");
-    for(int i = 0; i < 1024; ++i) {
-        assert(test_arr_seq[i] == i);
-    }
-    printf("<======== 0x512d8088 check thread task do done\n");
-
-    return 0;
-}
-
-
 int main(int argc, char *argv[])
 {
-    int ret = test_thread_number() || test_thread_seq();
+    int ret = test_thread_number());
 
     return ret;
 }
